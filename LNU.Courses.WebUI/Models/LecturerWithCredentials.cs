4956a7556854941348a70a2c0ea9a518d09c39a1
Andrew Zvarych <fipsher@gmail.com>
2016-10-22 16:10:58 +0300

    Fix ui

--- LNU.Courses.WebUI/Models/LecturerWithCredentials.cs
+++ LNU.Courses.WebUI/Models/LecturerWithCredentials.cs
@@ -10,12 +10,16 @@ namespace LNU.Courses.WebUI.Models
     {
         [MinLength(3, ErrorMessage = "Мінімальна довжина повинна бути 3")]
         [Required(ErrorMessage = "Обов'язково до заповнення")]
+        [Display(Name ="П.І.Б")]
         public string fullName { get; set; }
+        [Display(Name ="Номер телефону")]
         [Required(ErrorMessage = "Обов'язково до заповнення")]
         public string phone { get; set; }
+        [Display(Name ="Лоігн")]
         [MinLength(3, ErrorMessage = "Мінімальна довжина повинна бути 3")]
         [Required(ErrorMessage = "Обов'язково до заповнення")]
         public string login { get; set; }
+        [Display(Name ="Пароль")]
         [MinLength(3, ErrorMessage = "Мінімальна довжина повинна бути 3")]
         [Required(ErrorMessage = "Обов'язково до заповнення")]
         public string password { get; set; }
